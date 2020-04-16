55e2b94e66cc7b0515f1406d3dd40738b23e5daa
Akayeshmantha <akayeshmantha@apache.org>
2020-04-16 22:57:15 +0200

    remove rocket chat.

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/system/dto/rocketchat/sso/RocketChatResponse.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/system/dto/rocketchat/sso/RocketChatResponse.java
@@ -1,13 +1,13 @@
-package eu.nimble.core.infrastructure.identity.system.dto.rocketchat.sso;
-
-public class RocketChatResponse {
-    private String loginToken;
-
-    public String getLoginToken() {
-        return loginToken;
-    }
-
-    public void setLoginToken(String loginToken) {
-        this.loginToken = loginToken;
-    }
-}
+//package eu.nimble.core.infrastructure.identity.system.dto.rocketchat.sso;
+//
+//public class RocketChatResponse {
+//    private String loginToken;
+//
+//    public String getLoginToken() {
+//        return loginToken;
+//    }
+//
+//    public void setLoginToken(String loginToken) {
+//        this.loginToken = loginToken;
+//    }
+//}
